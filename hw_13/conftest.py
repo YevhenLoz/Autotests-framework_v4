d1a23f61043d65b4b2da0b7226bb7c6f168bca46
Yevhen L <yevhen.lozovatslyi@gmail.com>
2022-11-28 14:04:36 +0200

    hw14

--- hw_13/conftest.py
+++ hw_13/conftest.py
@@ -1,13 +1,17 @@
+import json
 import pytest
-
+from hw_13.CONSTANTS import ROOT_DIR
 from hw_13.page_objects.blog_page.blog_page import BlogPage
 from hw_13.page_objects.login_page.login_page import LoginLink
+from hw_13.utilities.configuration import Configuration
 from hw_13.utilities.driver_factory import DriverFactory
-from hw_13.utilities.read_configs import ReadConfig
 
 
 def pytest_configure(config):
-    # register an additional marker
+    """
+    This function register an additional marker
+    :param config:
+    """
     config.addinivalue_line(
         "markers", 'regression: for regression'
     )
@@ -16,20 +20,50 @@ def pytest_configure(config):
     )
 
 
-@pytest.fixture()
-def create_driver():
-    driver_chrome = DriverFactory.create_driver(ReadConfig.get_browser_id())
-    driver_chrome.maximize_window()
-    driver_chrome.get(ReadConfig.get_base_url())
-    yield driver_chrome
-    driver_chrome.quit()
+@pytest.fixture(scope="session")
+def env():
+    """
+    This fixture gives access to configuraton.json
+    :return: dict
+    """
+    with open(f'{ROOT_DIR}/configurations/configuration.json') as f:
+        data = f.read()
+        json_to_dict = json.loads(data)
+    configs = Configuration(**json_to_dict)
+    return configs
+
+
+@pytest.fixture
+def create_driver(env):
+    """
+    This fixture creates friver
+    :param env: base_url
+    :return: driver
+    """
+    browser = env.browser_id
+    get_base_url = env.base_url
+    driver = DriverFactory.create_driver(browser)
+    driver.maximize_window()
+    driver.get(get_base_url)
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture()
 def open_login_link(create_driver):
+    """
+    This fixture openes login page
+    :param create_driver:
+    :return: LoginLink
+    """
     return LoginLink(create_driver)
 
 
 @pytest.fixture()
 def open_blog_page(create_driver):
+    """
+    This fixture openes blog page
+    :param create_driver:
+    :return: BlogPage
+    """
     return BlogPage(create_driver)
