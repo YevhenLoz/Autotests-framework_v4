d7272a8c877841149ad689749c12fa1068a0e997
YevhenLoz <98827082+YevhenLoz@users.noreply.github.com>
2022-12-24 15:56:55 +0200

    Update driver_factory.py

--- hw_13/utilities/driver_factory.py
+++ hw_13/utilities/driver_factory.py
@@ -17,6 +17,7 @@ class DriverFactory:
             chrome_options = Options()
             if is_headless:
                 chrome_options.add_argument("--headless")
+                chrome_options.add_argument("--no-sendbox")
             driver = Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         elif int(driver_id) == DriverFactory.FIREFOX:
             driver = Firefox(service=Service(GeckoDriverManager().install()))
