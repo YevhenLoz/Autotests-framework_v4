7ec1101447cc260e19016c1c0910b262dcab2140
YevhenLoz <98827082+YevhenLoz@users.noreply.github.com>
2022-12-25 12:13:31 +0200

    Update driver_factory.py

--- hw_13/utilities/driver_factory.py
+++ hw_13/utilities/driver_factory.py
@@ -5,6 +5,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.chrome.options import Options
 
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 
 class DriverFactory:
     CHROME = 1
@@ -16,8 +17,11 @@ class DriverFactory:
         if int(driver_id) == DriverFactory.CHROME:
             chrome_options = Options()
             if is_headless:
+                chrome_options.add_argument(f'user-agent={user_agent}')
                 chrome_options.add_argument("--headless")
                 chrome_options.add_argument("--no-sandbox")
+                chrome_options.add_argument('--allow-running-insecure-content')
+                chrome_options.add_argument("--window-size=1920x1080")
             driver = Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         elif int(driver_id) == DriverFactory.FIREFOX:
             driver = Firefox(service=Service(GeckoDriverManager().install()))
