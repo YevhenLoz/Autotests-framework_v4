15abb4da03b0f56a1ced5b29b8b1b13a60e71bb3
YevhenLoz <98827082+YevhenLoz@users.noreply.github.com>
2022-12-26 11:25:19 +0200

    Update test_login.py

--- hw_13/tests/test_login.py
+++ hw_13/tests/test_login.py
@@ -8,28 +8,28 @@ def test_login(open_login_link, env):
     assert login_link.is_logout_link_visible() is True
 
 
-@pytest.mark.skip('Skipped test')
+
 def test_login_empty_pass(open_login_link, env):
     login_link = open_login_link
     login_link = login_link.login_with_empty_password(env.email)
     assert login_link.is_required_password_visible() is True
 
 
-@pytest.mark.skip('Skipped test')
+
 def test_login_no_creds(open_login_link):
     login_link = open_login_link
     login_link = login_link.login_without_creds()
     assert login_link.is_required_password_visible() and login_link.is_required_email_visible() is True
 
 
-@pytest.mark.skip('Skipped test')
+
 def test_login_empty_email(open_login_link, env):
     login_link = open_login_link
     login_link = login_link.login_with_empty_email(env.password)
     assert login_link.is_required_email_visible() is True
 
 
-@pytest.mark.skip('Skipped test')
+
 def test_logout(open_login_link, env):
     login_link = open_login_link
     logout_link = login_link.logout(env.email, env.password)
