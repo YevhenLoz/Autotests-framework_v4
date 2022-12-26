9e09a3ecc27d45af3ea6e49e8fc802631d71d69a
YevhenLoz <98827082+YevhenLoz@users.noreply.github.com>
2022-12-26 11:24:09 +0200

    Update test_blog_page.py

--- hw_13/tests/test_blog_page.py
+++ hw_13/tests/test_blog_page.py
@@ -14,7 +14,7 @@ def test_post_list_visible(open_blog_page):
     assert open_blog.is_post_list_visible() is True
 
 
-@pytest.mark.skip('Skipped test')
+
 def test_side_bar_visible(open_blog_page):
     open_blog = open_blog_page
     open_blog = open_blog.go_to_blog()
